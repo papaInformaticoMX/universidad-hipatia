8bccd1d3c5c0f7a978bcc8da28ffd967a01fdcc3
Papa Informatico <papainformaticomx@gmail.com>
2025-06-02 21:22:01 -0600

    Adding @angular/ssr
    Created a module AppServerModule
    
    Created a file main.server.ts
    
    Created a file config server.ts
    
    Configure scripts package.json forbuild y serve SSR

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,9 +4,10 @@ import { provideRouter } from '@angular/router';
 import { appRoutes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { provideHttpClient } from '@angular/common/http';
+import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
 
 export const appConfig: ApplicationConfig = {
 
   providers: [provideHttpClient(),
-    provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(appRoutes), provideAnimationsAsync()]
+    provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(appRoutes), provideAnimationsAsync(), provideClientHydration(withEventReplay())]
 };
